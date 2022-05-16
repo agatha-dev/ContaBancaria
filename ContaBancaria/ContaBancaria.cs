22f4d7ac62218b4eda3babf169c2f544d8595295
Ágatha almeida <agathasantos145@gmail.com>
2022-05-16 00:00:49 -0300

    Adicionar arquivos de projeto.

---
+++ ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BancoSistema;
+
+internal class ContaBancaria : Conta
+{
+    private int numero;
+    private double depositoInicial;
+
+    public ContaBancaria(int numero, string? titular)
+    {
+        this.numero = numero;
+        Titular = titular;
+    }
+
+    public ContaBancaria(int numero, string? titular, double depositoInicial)
+    {
+        this.numero = numero;
+        Titular = titular;
+        this.depositoInicial = depositoInicial;
+    }
+}
